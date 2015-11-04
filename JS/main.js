d77ad45f2d0268a110fac9f50cee248521c9f72a
Taylor Preston <taylor.preston.music@gmail.com>
2015-11-03 22:14:56 -0600

    inital commit

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+console.log(Backbone)
+
+
+
+var GameView = Backbone.View.extend({
+  tagName: 'section',
+  className: 'gamePortal',
+  // settingsTemplate: _.template($('#settingsTemplate').html()),
+  // leaderBoardTemplate: _.template($('#leaderBoardTemplate').html()),
+  // //
+  // render: function(){
+  //   this.$el.html(this.template)
+  // },
+})
+
+
+var GameRouter = Backbone.Router.extend({
+  routes: {
+    '': 'main',
+    'play': 'play',
+    'leaderBoard': 'leaderBoard',
+    'settings': 'settings',
+  },
+
+  main: function(){
+    setTimeout(function(){
+    $('.gameDisplay').html('');
+  },3000)
+  },
+  play: function(){
+    $('.gameDisplay').html('');
+  },
+  leaderBoard: function(){
+    $('.gameDisplay').html('');
+    $('.gameDisplay').html(_.template($('#leaderBoardTemplate').html()))
+  },
+  settings: function(){
+    $('.gameDisplay').html('');
+    $('.gameDisplay').html(_.template($('#settingsTemplate').html()));
+  },
+});
+
+new GameRouter()
+
+var PlayView = new GameView()
+
+Backbone.history.start();
